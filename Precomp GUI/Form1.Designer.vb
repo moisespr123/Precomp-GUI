101e51c2bd2a6e6d29d14b9fa88e253bf9cf04ce
moisesmcardona <moises@moises-studios.com>
2019-04-21 08:10:51 -0400

    Add project files.

---
+++ Precomp GUI/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Step1label = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RecompressRadiobutton = New System.Windows.Forms.RadioButton()
+        Me.PrecompressRadiobutton = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Step2label = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Step1label
+        '
+        Me.Step1label.AutoSize = True
+        Me.Step1label.Location = New System.Drawing.Point(12, 70)
+        Me.Step1label.Name = "Step1label"
+        Me.Step1label.Size = New System.Drawing.Size(197, 13)
+        Me.Step1label.TabIndex = 0
+        Me.Step1label.Text = "Step 1: Browse for a file to precompress:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RecompressRadiobutton)
+        Me.GroupBox1.Controls.Add(Me.PrecompressRadiobutton)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(202, 51)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Action"
+        '
+        'RecompressRadiobutton
+        '
+        Me.RecompressRadiobutton.AutoSize = True
+        Me.RecompressRadiobutton.Location = New System.Drawing.Point(98, 19)
+        Me.RecompressRadiobutton.Name = "RecompressRadiobutton"
+        Me.RecompressRadiobutton.Size = New System.Drawing.Size(84, 17)
+        Me.RecompressRadiobutton.TabIndex = 1
+        Me.RecompressRadiobutton.TabStop = True
+        Me.RecompressRadiobutton.Text = "Recompress"
+        Me.RecompressRadiobutton.UseVisualStyleBackColor = True
+        '
+        'PrecompressRadiobutton
+        '
+        Me.PrecompressRadiobutton.AutoSize = True
+        Me.PrecompressRadiobutton.Location = New System.Drawing.Point(6, 19)
+        Me.PrecompressRadiobutton.Name = "PrecompressRadiobutton"
+        Me.PrecompressRadiobutton.Size = New System.Drawing.Size(86, 17)
+        Me.PrecompressRadiobutton.TabIndex = 0
+        Me.PrecompressRadiobutton.TabStop = True
+        Me.PrecompressRadiobutton.Text = "Precompress"
+        Me.PrecompressRadiobutton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 87)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(209, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(230, 85)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(230, 135)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 137)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(209, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Step2label
+        '
+        Me.Step2label.AutoSize = True
+        Me.Step2label.Location = New System.Drawing.Point(12, 120)
+        Me.Step2label.Name = "Step2label"
+        Me.Step2label.Size = New System.Drawing.Size(293, 13)
+        Me.Step2label.TabIndex = 4
+        Me.Step2label.Text = "Step 2: Browse for a location to save the precompressed file:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Location = New System.Drawing.Point(15, 163)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(209, 50)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Recompression mode:"
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(134, 19)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(50, 17)
+        Me.RadioButton5.TabIndex = 2
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Brute"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(70, 19)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(60, 17)
+        Me.RadioButton4.TabIndex = 1
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Intense"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(6, 19)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton3.TabIndex = 0
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Normal"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(15, 219)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(290, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Step2label)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Step1label)
+        Me.Name = "Form1"
+        Me.Text = "Precomp GUI"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Step1label As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RecompressRadiobutton As RadioButton
+    Friend WithEvents PrecompressRadiobutton As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Step2label As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Precomp GUI/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub PrecompressRadiobutton_CheckedChanged(sender As Object, e As EventArgs) Handles PrecompressRadiobutton.CheckedChanged
+        If PrecompressRadiobutton.Checked Then
+            Step1label.Text = "Step 1: Browse for a file to precompress:"
+            Step2label.Text = "Step 2: Browse for a location to save the precompressed file:"
+        End If
+    End Sub
+
+    Private Sub RecompressRadiobutton_CheckedChanged(sender As Object, e As EventArgs) Handles RecompressRadiobutton.CheckedChanged
+        If RecompressRadiobutton.Checked Then
+            Step1label.Text = "Step 1: Browse for a file to recompress:"
+            Step2label.Text = "Step 2: Browse for a location to save the recompressed file:"
+        End If
+    End Sub
+End Class
